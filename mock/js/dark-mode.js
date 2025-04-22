5d15b7fb11bde2c1033d7fb6bd96e8950c596294
“Nahuel <nnahuell.gb@hotmail.com>
2025-04-22 01:44:30 -0300

    part3

--- mock/js/dark-mode.js
+++ mock/js/dark-mode.js
@@ -1,3 +1,5 @@
-document.getElementById('modo-desktop').addEventListener('change', function () {
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('modo-desktop').addEventListener('change', function () {
     document.body.classList.toggle('dark-mode');
   });
+});
