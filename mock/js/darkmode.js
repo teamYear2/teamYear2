27f04319032752313218c96c27222efebb7f5a89
EmilioR <emiliosoad@gmail.com>
2025-06-06 10:44:31 -0300

    cambios PortEmilio

--- mock/js/darkmode.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('modo-desktop').addEventListener('change', function () {
-        document.body.classList.toggle('dark-mode');
-    });
-});
