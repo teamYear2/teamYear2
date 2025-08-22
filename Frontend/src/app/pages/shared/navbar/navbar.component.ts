cc8ef36c9b1d4b598333d090e4b528ee79d01ca9
CHRISTIANDAGATTI <dagattichristian@gmail.com>
2025-08-22 17:39:00 -0300

    Html y css de maquetado a Angular - corregido

--- Frontend/src/app/pages/shared/navbar/navbar.component.ts
+++ Frontend/src/app/pages/shared/navbar/navbar.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   imports: [],
-  templateUrl: './navbar.html',
-  styleUrl: './navbar.css'
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
 })
 export class Navbar {
 
