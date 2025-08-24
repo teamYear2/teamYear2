4934e45e83c74bf0f6a88835cf3237dc8af1c296
EmilioR <emiliosoad@gmail.com>
2025-08-24 13:14:43 -0300

    add routerModule Registro

--- Frontend/src/app/pages/registro/registro.ts
+++ Frontend/src/app/pages/registro/registro.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './registro.html',
   styleUrl: './registro.css'
 })
