32cf2cffa4adc6ea0a5a2d273b011f283a8f7aab
EmilioR <emiliosoad@gmail.com>
2025-08-24 22:24:57 -0300

    formReactivo Registro

--- Frontend/src/app/pages/registro/registro.ts
+++ Frontend/src/app/pages/registro/registro.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
-  imports: [RouterModule],
+  imports: [RouterModule, ReactiveFormsModule],
   templateUrl: './registro.html',
   styleUrl: './registro.css'
 })
 export class Registro {
 
+  formRegistro!: FormGroup;
+  mostrarToast: boolean = false;
+  mensajeToast: string = '';
+  tipoToast: 'success' | 'danger' = 'success';
+
+  constructor(private formbuilder: FormBuilder) {
+
+    this.formRegistro = this.formbuilder.group(
+      {
+        nombreCompleto: ['', [Validators.required, Validators.minLength(3)]],
+        correo: ['', [Validators.required, Validators.email]],
+        contrasena: ['', [Validators.required, Validators.minLength(6)]],
+        confirmarContrasena: ['', [Validators.required]]
+      },
+      {
+        validators: this.passwordsIgualesValidator
+      }
+    )
+  }
+
+  passwordsIgualesValidator(group: AbstractControl): ValidationErrors | null {
+    const pass = group.get('contrasena')?.value;
+    const confirm = group.get('confirmarContrasena')?.value;
+    return pass === confirm ? null : { contrasenasNoCoinciden: true };
+  }
+
+  registrar() {
+    if (this.formRegistro.valid) {
+      this.mensajeToast = '¡Registro exitoso!';
+      this.tipoToast = 'success';
+    } else {
+      this.mensajeToast = 'Formulario inválido. Por favor revisa los campos.';
+      this.tipoToast = 'danger';
+    }
+
+    this.mostrarToast = true;
+
+    setTimeout(() => this.mostrarToast = false, 3000);
+  }
 }
