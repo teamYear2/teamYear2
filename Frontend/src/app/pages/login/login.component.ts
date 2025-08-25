2efdb232b06b2459af91966580196013ed090d5f
CHRISTIANDAGATTI <dagattichristian@gmail.com>
2025-08-24 21:08:23 -0300

    Form reactivo login

--- Frontend/src/app/pages/login/login.component.ts
+++ Frontend/src/app/pages/login/login.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterModule],
+  imports: [RouterModule, ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class Login {}
+export class Login {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Formulario válido:', this.loginForm.value);
+    } else {
+      console.log('Formulario inválido');
+      this.markFormGroupTouched();
+    }
+  }
+
+  private markFormGroupTouched() {
+    Object.keys(this.loginForm.controls).forEach(key => {
+      const control = this.loginForm.get(key);
+      control?.markAsTouched();
+    });
+  }
+
+  // Métodos para obtener errores específicos del formulario reactivo
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+}
