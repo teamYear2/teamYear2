4e58134a872b349514b2da0477bf30b82e85f964
SrLachy <ignacioareosa07@gmail.com>
2025-09-08 19:25:30 -0300

    add: mejoras al home (se añadió hero, features-carrusel y una section CTA)

---
+++ Frontend/src/app/pages/home/shared/cta-section/cta-section.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-cta-section',
+  imports: [RouterModule],
+  templateUrl: './cta-section.html',
+  styleUrl: './cta-section.css'
+})
+export class CtaSection {
+
+}
