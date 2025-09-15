5c7432b84976383f30114dbb20fd2116b981f753
SrLachy <ignacioareosa07@gmail.com>
2025-09-14 23:25:53 -0300

    fix: error de importaciones

--- Frontend/src/app/pages/home/shared/contact-form/contact-form.ts
+++ Frontend/src/app/pages/home/shared/contact-form/contact-form.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contact-form.html',
   styleUrl: './contact-form.css',
 })
